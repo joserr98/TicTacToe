3d61ab429c338a33121076267ff0dd983284a754
joserr98 <joserr9807@gmail.com>
2023-04-08 02:22:26 +0200

    Añadir jugabilidad de la IA (nivel 0)

--- dist/main.js
+++ dist/main.js
@@ -4,8 +4,12 @@ let player1Turns = 3;
 let player2Turns = 3;
 let currentPlayer = "x";
 let lastQuit;
+let ficha = 'luffy';
 let gameOver = false;
 const views = document.querySelectorAll(".container-fluid");
+let nickname;
+let nickname2;
+let nick;
 let currentViewIndex = 0;
 var regex = "/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:.[a-zA-Z0-9-]+)*$/";
 const ticTacToeBoard = [
@@ -47,14 +51,26 @@ const checkWin = () => {
 };
 const cambiarTurno = () => {
     currentPlayer == "x" ? (currentPlayer = "o") : (currentPlayer = "x");
+    if (currentPlayer === "x") {
+        ficha = 'luffy';
+        nick = nickname;
+    }
+    else {
+        ficha = 'chopper';
+        nick = nickname2;
+    }
 };
 const reducirContador = () => {
+    let turns;
     if (currentPlayer === "x") {
         player1Turns = player1Turns - 1;
+        turns = player1Turns;
     }
     else {
         player2Turns = player2Turns - 1;
+        turns = player2Turns;
     }
+    return turns;
 };
 const aumentarContador = () => {
     if (currentPlayer === "x") {
@@ -64,53 +80,72 @@ const aumentarContador = () => {
         player2Turns = player2Turns + 1;
     }
 };
-const colocarFicha = (fila, columna, celda) => {
-    const box = document.querySelector(`#box${celda}`);
+const colocarFicha = (cell) => {
+    const box = document.querySelector(`#${cell}`);
+    const row = document.querySelector(`#${cell}`).dataset.row;
+    const column = document.querySelector(`#${cell}`).dataset.column;
     let turnos = comprobarTurnos();
-    //   if (false) {
-    //     if (currentPlayer) {
-    //       if (turnos > 0) {
-    //         if (box.innerHTML === "" && box != lastQuit) {
-    //           ticTacToeBoard[fila][columna] = currentPlayer;
-    //           box.innerHTML = currentPlayer;
-    //           reducirContador();
-    //           let winner = checkWin();
-    //           if (winner) {
-    //             clearTablero();
-    //           }
-    //           cambiarTurno();
-    //         }
-    //       } else {
-    //         if (box.innerHTML !== "" && box.innerHTML === currentPlayer) {
-    //           lastQuit = box;
-    //           ticTacToeBoard[fila][columna] = "";
-    //           box.innerHTML = "";
-    //           aumentarContador();
-    //         }
-    //       }
-    //     }
-    //   } else {
-    if (turnos > 0) {
-        if (box.innerHTML === "" && box != lastQuit) {
-            ticTacToeBoard[fila][columna] = currentPlayer;
-            box.innerHTML = currentPlayer;
-            reducirContador();
-            let winner = checkWin();
-            if (winner) {
-                clearTablero();
+    if (true) {
+        if (currentPlayer) {
+            if (turnos > 0) {
+                if (ticTacToeBoard[row][column] === '' && box != lastQuit) {
+                    ticTacToeBoard[row][column] = currentPlayer;
+                    box.innerHTML = currentPlayer;
+                    box.classList.add(`${ficha}`);
+                    let turns = reducirContador();
+                    let winner = checkWin();
+                    if (winner) {
+                        clearTablero();
+                    }
+                    cambiarTurno();
+                    document.querySelector('.turn-name').innerHTML = `Turno de ${nick}`;
+                    document.querySelector('.turn-counter').innerHTML = `Te quedan ${turnos} turnos`;
+                }
             }
-            IAPlays();
+            else {
+                if (box.innerHTML !== "" && box.innerHTML === currentPlayer) {
+                    lastQuit = box;
+                    ticTacToeBoard[row][column] = "";
+                    box.innerHTML = "";
+                    box.classList.remove(`${ficha}`);
+                    document.querySelector('.turn-counter').innerHTML = `Te quedan ${turnos} turnos`;
+                    aumentarContador();
+                }
+            }
+        }
+    }
+    else {
+        if (turnos > 0) {
+            if (ticTacToeBoard[row][column] === '' && box != lastQuit) {
+                ticTacToeBoard[row][column] = currentPlayer;
+                box.classList.add(`${ficha}`);
+                reducirContador();
+                let winner = checkWin();
+                if (winner) {
+                    //   clearTablero();
+                    alert('Ganador ');
+                    return;
+                }
+                AIMovement(ticTacToeBoard);
+                let winnerAI = checkWin();
+                if (winnerAI) {
+                    //   clearTablero();
+                    alert('Ganador IA');
+                }
+            }
         }
         else {
             if (box.innerHTML !== "" && box.innerHTML === currentPlayer) {
                 lastQuit = box;
-                ticTacToeBoard[fila][columna] = "";
+                ticTacToeBoard[row][column] = "";
                 box.innerHTML = "";
+                box.classList.remove(`${ficha}`);
                 aumentarContador();
+                // AIMovement(ticTacToeBoard);
             }
         }
     }
-    //   }
+    console.log(player2Turns);
 };
 const comprobarTurnos = () => {
     if (currentPlayer === "x") {
@@ -129,6 +164,8 @@ const clearTablero = () => {
     let cell = document.querySelectorAll(".cell");
     for (let i = 0; i < cell.length; i++) {
         cell[i].innerHTML = "";
+        cell[i].classList.remove('luffy');
+        cell[i].classList.remove('chopper');
     }
     player1Turns = 3;
     player2Turns = 3;
@@ -136,8 +173,12 @@ const clearTablero = () => {
 const nextPage = (page) => {
     let views = document.querySelectorAll(".container-fluid");
     let actualPage = document.querySelector(`.${page}`);
-    let nickname = document.querySelector("#nickname").value;
-    let nickname2 = document.querySelector("#nickname2").value;
+    nickname = document.querySelector("#nickname").value;
+    nickname2 = document.querySelector("#nickname2").value;
+    document.querySelector('#player1').innerHTML = nickname;
+    document.querySelector('#player2').innerHTML = nickname2;
+    document.querySelector('.turn-name').innerHTML = `Turno de ${nickname}`;
+    document.querySelector('.turn-counter').innerHTML = `Te quedan ${player1Turns} turnos`;
     if (page === "game") {
         if (document.querySelector("#select-num-players1").checked === true) {
             if (nickname === "") {
@@ -185,46 +226,59 @@ const disableSecondPlayer = () => {
         nickname2.disabled = false;
     }
 };
-const IAPlays = () => {
-    let a = Math.floor(Math.random() * 3);
-    let b = Math.floor(Math.random() * 3);
-    let celda = 0;
-    if (a === 0) {
-        if (b === 0) {
-            celda = 1;
-        }
-        if (b === 1) {
-            celda = 2;
-        }
-        if (b === 2) {
-            celda = 3;
+function emptyPositions(arr) {
+    let emptyPositions = [];
+    for (let i = 0; i < ticTacToeBoard.length; i++) {
+        for (let j = 0; j < ticTacToeBoard.length; j++) {
+            if (ticTacToeBoard[i][j] == '') {
+                emptyPositions.push([i, j]);
+            }
         }
     }
-    if (a === 1) {
-        if (b === 0) {
-            celda = 4;
-        }
-        if (b === 1) {
-            celda = 5;
-        }
-        if (b === 2) {
-            celda = 6;
-        }
-    }
-    if (a === 2) {
-        if (b === 0) {
-            celda = 7;
-        }
-        if (b === 1) {
-            celda = 8;
-        }
-        if (b === 2) {
-            celda = 9;
+    return emptyPositions;
+}
+function OPositions(arr) {
+    let OPositions = [];
+    for (let i = 0; i < ticTacToeBoard.length; i++) {
+        for (let j = 0; j < ticTacToeBoard.length; j++) {
+            if (ticTacToeBoard[i][j] == 'o') {
+                OPositions.push([i, j]);
+            }
         }
     }
-    if (document.querySelector(`#box${celda}`).innerHTML === '') {
-        ticTacToeBoard[a][b] = 'o';
-        document.querySelector(`#box${celda}`).innerHTML = 'o';
+    return OPositions;
+}
+const AIMovement = (arr) => {
+    if (player2Turns > 0) {
+        const posicionesVacias = emptyPositions(arr);
+        const indiceAleatorio = Math.floor(Math.random() * posicionesVacias.length);
+        const posicionAleatoria = posicionesVacias[indiceAleatorio];
+        arr[posicionAleatoria[0]][posicionAleatoria[1]] = 'o';
+        let cells = document.querySelectorAll('.cell');
+        const arrayPositions = [];
+        for (let i = 0; i < cells.length; i++) {
+            arrayPositions.push(cells[i]);
+        }
+        let posiciones123 = Array.from({ length: 3 }, (_, i) => arrayPositions.slice(i * 3, (i + 1) * 3));
+        // posiciones123[posicionAleatoria[0]][posicionAleatoria[1]].innerHTML = 'o';
+        posiciones123[posicionAleatoria[0]][posicionAleatoria[1]].classList.add('chopper');
+        player2Turns--;
+    }
+    else {
+        const posicionesO = OPositions(arr);
+        const indiceAleatorio = Math.floor(Math.random() * posicionesO.length);
+        const posicionO = posicionesO[indiceAleatorio];
+        arr[posicionO[0]][posicionO[1]] = '';
+        let cells = document.querySelectorAll('.cell');
+        const arrayPositions = [];
+        for (let i = 0; i < cells.length; i++) {
+            arrayPositions.push(cells[i]);
+        }
+        let posiciones123 = Array.from({ length: 3 }, (_, i) => arrayPositions.slice(i * 3, (i + 1) * 3));
+        posiciones123[posicionO[0]][posicionO[1]].innerHTML = '';
+        posiciones123[posicionO[0]][posicionO[1]].classList.remove('chopper');
+        player2Turns++;
+        AIMovement(arr);
     }
 };
 //# sourceMappingURL=main.js.map
--- js/main.ts
+++ js/main.ts
@@ -5,8 +5,13 @@ let player1Turns = 3;
 let player2Turns = 3;
 let currentPlayer: fichas = "x";
 let lastQuit;
+let ficha: String = 'luffy';
 let gameOver = false;
 const views = document.querySelectorAll(".container-fluid");
+let nickname
+let nickname2
+let nick 
+
 let currentViewIndex = 0;
 var regex =
   "/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:.[a-zA-Z0-9-]+)*$/";
@@ -60,14 +65,25 @@ const checkWin = () => {
 
 const cambiarTurno = () => {
   currentPlayer == "x" ? (currentPlayer = "o") : (currentPlayer = "x");
+  if(currentPlayer === "x"){
+    ficha = 'luffy'
+    nick = nickname
+  } else {
+    ficha = 'chopper'
+    nick = nickname2
+  }
 };
 
 const reducirContador = () => {
+  let turns
   if (currentPlayer === "x") {
     player1Turns = player1Turns - 1;
+    turns = player1Turns
   } else {
     player2Turns = player2Turns - 1;
+    turns = player2Turns
   }
+  return turns
 };
 
 const aumentarContador = () => {
@@ -78,53 +94,70 @@ const aumentarContador = () => {
   }
 };
 
-const colocarFicha = (fila: number, columna: number, celda: number) => {
-  const box = document.querySelector(`#box${celda}`);
+const colocarFicha = (cell: string) => {
+  const box = document.querySelector(`#${cell}`);
+  const row = (document.querySelector(`#${cell}`) as any).dataset.row
+  const column = (document.querySelector(`#${cell}`) as any).dataset.column
   let turnos = comprobarTurnos();
-//   if (false) {
-//     if (currentPlayer) {
-//       if (turnos > 0) {
-//         if (box.innerHTML === "" && box != lastQuit) {
-//           ticTacToeBoard[fila][columna] = currentPlayer;
-//           box.innerHTML = currentPlayer;
-//           reducirContador();
-//           let winner = checkWin();
-//           if (winner) {
-//             clearTablero();
-//           }
-//           cambiarTurno();
-//         }
-//       } else {
-//         if (box.innerHTML !== "" && box.innerHTML === currentPlayer) {
-//           lastQuit = box;
-//           ticTacToeBoard[fila][columna] = "";
-//           box.innerHTML = "";
-//           aumentarContador();
-//         }
-//       }
-//     }
-//   } else {
-    if (turnos > 0) {
-        if (box.innerHTML === "" && box != lastQuit) {
-          ticTacToeBoard[fila][columna] = currentPlayer;
+  if (true) {
+    if (currentPlayer) {
+      if (turnos > 0) {
+        if (ticTacToeBoard[row][column] === '' && box != lastQuit) {
+          ticTacToeBoard[row][column] = currentPlayer;
           box.innerHTML = currentPlayer;
-          reducirContador();
+          box.classList.add(`${ficha}`)
+          let turns = reducirContador();
           let winner = checkWin();
           if (winner) {
             clearTablero();
           }
-          IAPlays();
-        } else {
-            if (box.innerHTML !== "" && box.innerHTML === currentPlayer) {
-              lastQuit = box;
-              ticTacToeBoard[fila][columna] = "";
-              box.innerHTML = "";
-              aumentarContador();
-            }
+          cambiarTurno();
+          document.querySelector('.turn-name').innerHTML = `Turno de ${nick}`
+          document.querySelector('.turn-counter').innerHTML = `Te quedan ${turnos} turnos`
         }
+      } else {
+        if (box.innerHTML !== "" && box.innerHTML === currentPlayer) {
+          lastQuit = box;
+          ticTacToeBoard[row][column] = "";
+          box.innerHTML = "";
+          box.classList.remove(`${ficha}`)
+          document.querySelector('.turn-counter').innerHTML = `Te quedan ${turnos} turnos`
+          aumentarContador();
+        }
+      }
     }
-//   }
-};
+  } else {
+    if (turnos > 0) {
+        if (ticTacToeBoard[row][column] === '' && box != lastQuit) {
+          ticTacToeBoard[row][column] = currentPlayer;
+          box.classList.add(`${ficha}`)
+          reducirContador();
+          let winner = checkWin();
+          if (winner) {
+          //   clearTablero();
+          alert('Ganador ')
+          return
+          }
+          AIMovement(ticTacToeBoard);
+          let winnerAI = checkWin();
+          if (winnerAI) {
+          //   clearTablero();
+            alert('Ganador IA')
+          }
+        } 
+    } else {
+      if (box.innerHTML !== "" && box.innerHTML === currentPlayer) {
+        lastQuit = box;
+        ticTacToeBoard[row][column] = "";
+        box.innerHTML = "";
+        box.classList.remove(`${ficha}`)
+        aumentarContador();
+        // AIMovement(ticTacToeBoard);
+      }
+  }
+  }
+  console.log(player2Turns)
+  };
 
 const comprobarTurnos = (): number => {
   if (currentPlayer === "x") {
@@ -143,17 +176,24 @@ const clearTablero = () => {
   let cell = document.querySelectorAll(".cell");
   for (let i = 0; i < cell.length; i++) {
     cell[i].innerHTML = "";
+    cell[i].classList.remove('luffy')
+    cell[i].classList.remove('chopper')
   }
   player1Turns = 3;
   player2Turns = 3;
 };
 
-const nextPage = (page) => {
+const nextPage = (page: string) => {
   let views = document.querySelectorAll(".container-fluid");
   let actualPage = document.querySelector(`.${page}`);
 
-  let nickname = (document.querySelector("#nickname") as any).value;
-  let nickname2 = (document.querySelector("#nickname2") as any).value;
+  nickname = (document.querySelector("#nickname") as any).value;
+  nickname2 = (document.querySelector("#nickname2") as any).value;
+
+  document.querySelector('#player1').innerHTML = nickname
+  document.querySelector('#player2').innerHTML = nickname2
+  document.querySelector('.turn-name').innerHTML = `Turno de ${nickname}`
+  document.querySelector('.turn-counter').innerHTML = `Te quedan ${player1Turns} turnos`
   if (page === "game") {
     if ((document.querySelector("#select-num-players1") as any).checked === true) {
       if (nickname === "") {
@@ -201,47 +241,70 @@ const disableSecondPlayer = () => {
   }
 };
 
-const IAPlays = () => {
-    let a = Math.floor(Math.random() * 3)
-    let b = Math.floor(Math.random() * 3)
-    let celda = 0
+function emptyPositions(arr) {
+  let emptyPositions: any = []
 
-    if(a === 0){
-        if(b === 0){
-            celda = 1
-        }
-        if(b === 1){
-            celda = 2
-        }
-        if(b === 2){
-            celda = 3
-        }
+  for (let i = 0; i < ticTacToeBoard.length; i++){
+    for (let j = 0; j < ticTacToeBoard.length; j++){
+      if(ticTacToeBoard[i][j] == ''){
+        emptyPositions.push([i,j]);
+      }
     }
-    if(a === 1){
-        if(b === 0){
-            celda = 4
-        }
-        if(b === 1){
-            celda = 5
-        }
-        if(b === 2){
-            celda = 6
-        }
+  }
+  return emptyPositions
+}
+
+function OPositions(arr) {
+  let OPositions: any = []
+
+  for (let i = 0; i < ticTacToeBoard.length; i++){
+    for (let j = 0; j < ticTacToeBoard.length; j++){
+      if(ticTacToeBoard[i][j] == 'o'){
+        OPositions.push([i,j]);
+      }
     }
-    if(a === 2){
-        if(b === 0){
-            celda = 7
-        }
-        if(b === 1){
-            celda = 8
-        }
-        if(b === 2){
-            celda = 9
-        }
+  }
+  return OPositions
+}
+
+const AIMovement = (arr) => {
+
+  if (player2Turns > 0){
+    const posicionesVacias = emptyPositions(arr);
+    const indiceAleatorio = Math.floor(Math.random() * posicionesVacias.length);
+    const posicionAleatoria = posicionesVacias[indiceAleatorio];
+    arr[posicionAleatoria[0]][posicionAleatoria[1]] = 'o';
+    let cells = document.querySelectorAll('.cell')
+    
+    const arrayPositions = []
+  
+    for (let i = 0; i < cells.length; i++){
+      arrayPositions.push(cells[i])
     }
-    if(document.querySelector(`#box${celda}`).innerHTML === ''){
-        ticTacToeBoard[a][b] = 'o';
-        document.querySelector(`#box${celda}`).innerHTML = 'o';
+  
+    let posiciones123 = Array.from({ length: 3 }, (_, i) => arrayPositions.slice(i * 3, (i + 1) * 3));
+    // posiciones123[posicionAleatoria[0]][posicionAleatoria[1]].innerHTML = 'o';
+    posiciones123[posicionAleatoria[0]][posicionAleatoria[1]].classList.add('chopper');
+    player2Turns--;
+  } else {
+    const posicionesO = OPositions(arr);
+    const indiceAleatorio = Math.floor(Math.random() * posicionesO.length);
+    const posicionO = posicionesO[indiceAleatorio];
+    arr[posicionO[0]][posicionO[1]] = '';
+
+    let cells = document.querySelectorAll('.cell')
+    
+    const arrayPositions = []
+  
+    for (let i = 0; i < cells.length; i++){
+      arrayPositions.push(cells[i])
     }
+
+    let posiciones123 = Array.from({ length: 3 }, (_, i) => arrayPositions.slice(i * 3, (i + 1) * 3));
+    posiciones123[posicionO[0]][posicionO[1]].innerHTML = '';
+    posiciones123[posicionO[0]][posicionO[1]].classList.remove('chopper');
+    player2Turns++;
+    AIMovement(arr)
+  }
 
 }
